5b0f0995e31e7096b208bef980166daaf9a502d9
Dion Pimentel <dionpimentel260@gmail.com>
2023-05-03 02:54:35 -0700

    Added folder creation

---
+++ Assets/TestFolderCreationOnBuild.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TestFolderCreationOnBuild : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!Directory.Exists(Application.dataPath + "/Game Decks"))
+        {
+            Directory.CreateDirectory(Application.dataPath + "/Game Decks");
+            Debug.LogAssertion("Folder created at " + Application.dataPath);
+
+        }
+        else
+        {
+            Debug.LogAssertion("Folder already exists in " + Path.GetDirectoryName(Application.dataPath + "/Game Decks"));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
